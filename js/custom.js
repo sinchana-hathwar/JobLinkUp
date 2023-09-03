3700e8eddcff31198888916daf796b99dc3e860c
sinchana-hathwar <sinchanar21@gmail.com>
2023-09-03 11:43:16 +0530

    Profile modal, read more button

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,22 @@ function getYear() {
 
 getYear();
 
+function readMore(){
+    let lesstxt =document.getElementById("less-txt");
+    let moretxt = document.getElementById("more-txt");
+    let btn = document.getElementById("read-more-btn");
+    if(lesstxt.style.display === "none"){
+        lesstxt.style.display = "block";
+        moretxt.style.display = "none";
+        btn.innerText = "Read More"
+    }
+    else{
+        lesstxt.style.display = "none";
+        moretxt.style.display = "block";
+        btn.innerText = "Read Less"
+    }
+}
+
 // nice select
 $(document).ready(function () {
     $('select').niceSelect();
